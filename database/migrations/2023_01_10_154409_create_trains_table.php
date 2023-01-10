682de794b15f0613f0752c6a6749b4220119c34e
saracassandra7 <saracassandra7@gmail.com>
2023-01-10 17:08:25 +0100

    [add] trains table and migration

---
+++ database/migrations/2023_01_10_154409_create_trains_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('agency', 50);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->string('train_code', 6);
+            $table->tinyInteger('carriages')->unsigned();
+            $table->tinyInteger('in_time')->unsigned()->default(1);
+            $table->tinyInteger('is_deleted')->unsigned()->default(0);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
